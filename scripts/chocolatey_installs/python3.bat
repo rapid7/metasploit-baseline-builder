16bc6c8303664e1c86e6327c57bb2d47bd149b58
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2018-01-17 16:28:39 -0600

    install specific python3 3.4.4 package

--- scripts/chocolatey_installs/python3.bat
+++ scripts/chocolatey_installs/python3.bat
@@ -1,11 +1,3 @@
 chocolatey feature enable -n=allowGlobalConfirmation
-choco install python --version 3.4.4
-
-REM -- Tesing if on windows 7, do not attempt to install 3.6.x as will hangs
-wmic os get caption | find /i "7" > NUL && set IS_WIN7=YES || set IS_WIN7=NO
-
-if %IS_WIN7%==NO (
-  choco install python --version 3.6.1
-)
-
+choco install python --version 3.4.4.20180111
 chocolatey feature disable -n=allowGlobalConfirmation
