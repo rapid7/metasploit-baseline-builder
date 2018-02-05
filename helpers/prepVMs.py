e886ffa00960ba7ad8d576aa7d2c5522ff228758
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2018-02-04 23:25:08 -0600

    expand prep for enable_autologin

--- helpers/prepVMs.py
+++ helpers/prepVMs.py
@@ -7,6 +7,9 @@ import time
 from tqdm import tqdm
 import vm_automation
 
+VM_USER = 'vagrant'
+VM_PASS = 'vagrant'
+
 WINDOWS_REQUIRED = "Win"
 UAC_ENABLE_COMMAND = ['cmd.exe',
                       '/k',
@@ -32,7 +35,40 @@ DISABLE_SMB1_COMMAND = ['cmd.exe',
                         '/d',
                         '0',
                         '/f']
-
+ENABLE_AUTOLOGIN_COMMAND_1 = ['cmd.exe',
+                              '/k',
+                              '%SystemRoot%\System32\\reg.exe',
+                              'ADD',
+                              '"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"',
+                              '/v',
+                              'DefaultUserName',
+                              '-t',
+                              'REG_SZ',
+                              '/d',
+                              VM_USER,
+                              '/f']
+ENABLE_AUTOLOGIN_COMMAND_2 = ['cmd.exe',
+                              '/k',
+                              '%SystemRoot%\System32\\reg.exe',
+                              'ADD',
+                              '"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"',
+                              '/v',
+                              'DefaultPassword',
+                              '-t',
+                              'REG_SZ',
+                              '/d',
+                              VM_PASS,
+                              '/f']
+ENABLE_AUTOLOGIN_COMMAND_3 = ['cmd.exe',
+                              '/k',
+                              '%SystemRoot%\System32\\reg.exe',
+                              'ADD',
+                              '"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"',
+                              '/v',
+                              'AutoAdminLogon',
+                              '/d',
+                              '1',
+                              '/f']
 
 
 def get_vm_server(config_file):
@@ -48,7 +84,7 @@ def get_vm_server(config_file):
     return None
 
 
-def execute_action(vm_config, vm_name, command):
+def execute_action(vm_config, vm_name, command_list):
     schedule_delay = 30
     vm_server = get_vm_server(config_file=vm_config)
     vm_server.enumerateVms()
@@ -58,9 +94,10 @@ def execute_action(vm_config, vm_name, command):
             vm_ready = False
             while vm_ready is False:
                 vm_ready = vm_server.waitForVmsToBoot([vm])
-            vm.setUsername('vagrant')
-            vm.setPassword('vagrant')
-            vm.runCmdOnGuest(command)
+            vm.setUsername(VM_USER)
+            vm.setPassword(VM_PASS)
+            for command in command_list:
+                vm.runCmdOnGuest(command)
             time.sleep(schedule_delay)
             vm.vmObject.ShutdownGuest()
             time.sleep(10)
@@ -70,22 +107,27 @@ def execute_action(vm_config, vm_name, command):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-k", "--keyword", help="VM search parameter")
-    parser.add_argument("-a", "--action", help="action [enable_uac|disable_smb1]")
+    parser.add_argument("-a", "--action", help="action [enable_uac|disable_smb1|enable_autologin]")
     parser.add_argument("-t", "--threads", help="Number of simultaneous threads", type=int)
     parser.add_argument("hypervisorConfig", help="json hypervisor config")
 
     args = parser.parse_args()
 
-    validActions = ['enable_uac', 'disable_smb1']
+    validActions = ['enable_uac', 'disable_smb1', 'enable_autologin']
 
     prefix = args.keyword
 
+    command_list = []
     if args.action.lower() not in validActions:
         print('INVALID ACTION')
     if args.action.lower() == 'enable_uac':
-        command = UAC_ENABLE_COMMAND
+        command_list.append(UAC_ENABLE_COMMAND)
     elif args.action.lower() == 'disable_smb1':
-        command = DISABLE_SMB1_COMMAND
+        command_list.append(DISABLE_SMB1_COMMAND)
+    elif args.action.lower() == 'enable_autologin':
+        command_list.append(ENABLE_AUTOLOGIN_COMMAND_1)
+        command_list.append(ENABLE_AUTOLOGIN_COMMAND_2)
+        command_list.append(ENABLE_AUTOLOGIN_COMMAND_3)
 
     num_threads = 3
     if args.threads is not None and args.threads > 0:
@@ -112,7 +154,7 @@ def main():
 
         results = []
         for vm_name in vm_list:
-            pool.apply_async(execute_action, [args.hypervisorConfig, vm_name, command], callback=results.append)
+            pool.apply_async(execute_action, [args.hypervisorConfig, vm_name, command_list], callback=results.append)
 
         with tqdm(total=len(vm_list)) as progress:
             current_len = 0
