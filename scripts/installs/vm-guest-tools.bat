ab350f45ee03e774c45466231586244de172cb2c
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2018-04-09 10:20:17 -0500

    update 7z msi version

--- scripts/installs/vm-guest-tools.bat
+++ scripts/installs/vm-guest-tools.bat
@@ -2,12 +2,12 @@ reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" 
 
 if %OS%==32BIT (
     if not exist "C:\Windows\Temp\7z920.msi" (
-        powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://www.7-zip.org/a/7z920.msi', 'C:\Windows\Temp\7z920.msi')" <NUL
+        powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://www.7-zip.org/a/7z1801.msi', 'C:\Windows\Temp\7z920.msi')" <NUL
     )
 )
 if %OS%==64BIT echo This is a 64bit operating system
     if not exist "C:\Windows\Temp\7z920.msi" (
-        powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://www.7-zip.org/a/7z920-x64.msi', 'C:\Windows\Temp\7z920.msi')" <NUL
+        powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://www.7-zip.org/a/7z1801-x64.msi', 'C:\Windows\Temp\7z920.msi')" <NUL
     )
 msiexec /qb /i C:\Windows\Temp\7z920.msi
 
