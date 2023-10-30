f80ef3f1e7d96cb5d36b8a9971180bfd6b92549c
sjanusz-r7 <Simon_Janusz@rapid7.com>
2023-10-30 17:28:05 +0000

    Install open-vm-tools

--- scripts/ubuntu_customs/ubuntu_vmware.sh
+++ scripts/ubuntu_customs/ubuntu_vmware.sh
@@ -6,7 +6,7 @@ function install_open_vm_tools {
     echo "==> Installing Open VM Tools"
     # Install open-vm-tools so we can mount shared folders
     #dpkg --configure -a
-    apt-get install -y open-vm-tools-lts-trusty
+    apt-get install -y open-vm-tools
     # Install open-vm-tools-desktop so we can copy/paste, resize, etc.
     if [[ "$DESKTOP" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
         apt-get install -y open-vm-tools-desktop
