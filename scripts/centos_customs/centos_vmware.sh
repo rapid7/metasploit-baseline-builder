5cda1fc09c4d1e8359b5fbde0cc00f96d15f7aef
sjanusz-r7 <Simon_Janusz@rapid7.com>
2023-11-01 17:12:13 +0000

    Use vault repos for CentOS 8

--- scripts/centos_customs/centos_vmware.sh
+++ scripts/centos_customs/centos_vmware.sh
@@ -3,10 +3,10 @@
 SSH_USER=${SSH_USERNAME:-vagrant}
 SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
 
-# Right now, only CentOS 6 needs the vault repos.
+# Right now, only CentOS 6 & 8 needs the vault repos.
 # In the future, this may need to be changed to CentOS 7 as well once that reaches EOL.
-cat /etc/centos-release
-if grep -q -i "release 6" /etc/redhat-release ; then
+# https://endoflife.date/centos
+if grep -q -i "release 6" /etc/centos-release || grep -q -i "release 8" /etc/centos-release ; then
   echo "==> Changing to vault repositories"
   sudo sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
   sudo sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
