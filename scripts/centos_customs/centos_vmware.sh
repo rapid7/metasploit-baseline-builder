9488a02e9bef7254e1a73d252140dbea40cd8755
sjanusz-r7 <Simon_Janusz@rapid7.com>
2023-11-01 16:18:35 +0000

    Move the CentOS 6 repository script to vmware setup as it runs first

--- scripts/centos_customs/centos_vmware.sh
+++ scripts/centos_customs/centos_vmware.sh
@@ -3,6 +3,15 @@
 SSH_USER=${SSH_USERNAME:-vagrant}
 SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
 
+# Right now, only CentOS 6 needs the vault repos.
+# In the future, this may need to be changed to CentOS 7 as well once that reaches EOL.
+cat /etc/centos-release
+if grep -q -i "release 6" /etc/redhat-release ; then
+  echo "==> Changing to vault repositories"
+  sudo sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
+  sudo sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
+fi
+
 function install_open_vm_tools {
     echo "==> Installing Open VM Tools"
     # Install open-vm-tools so we can mount shared folders
