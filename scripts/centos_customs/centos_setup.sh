99c3594b340d88a86eb5a74b274e97ceaf5d36ae
sjanusz-r7 <Simon_Janusz@rapid7.com>
2023-11-01 18:23:14 +0000

    Add powertools &python 2.7 setup for CentOS 8

--- scripts/centos_customs/centos_setup.sh
+++ scripts/centos_customs/centos_setup.sh
@@ -2,13 +2,34 @@
 
 echo 'LC_ALL="en_US.UTF-8"' >> /etc/default/locale
 
+# These packages are available on CentOS 6, 7 and 8.
 yum -y install \
   gcc gpg openssl-devel bzip2-devel \
   gcc-c++ patch readline readline-devel zlib zlib-devel \
-  libyaml-devel libffi-devel make \
+  libffi-devel make \
   bzip2 autoconf automake libtool bison sqlite-devel \
-  python-pip python curl gnupg2 && \
+  curl gnupg2 && \
 yum -y install java-1.8.0-openjdk.x86_64 && \
+
+# CentOS 8 doesn't have some of the packages that are present in 6 & 7.
+# e.g. python -> python2.7
+# CentOS 6 doesn't have issues with missing packages currently.
+# https://endoflife.date/centos
+if grep -q -i "release 8" /etc/centos-release ; then
+  # python2 installs python2.7 and pip, but pip isn't available in PATH.
+  # install it using the get-pip script to ensure it works.
+  yum -y install python2
+  # Install python-pip with a script instead, similar to what old Ubuntu versions do.
+  curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+  sudo python2.7 get-pip.py
+  # https://serverfault.com/questions/997896/how-to-enable-powertools-repository-in-centos-8
+  yum -y install dnf-plugins-core
+  yum config-manager --set-enabled powertools
+  yum -y install libyaml-devel
+else
+  yum -y install python python-pip libyaml-devel
+fi
+
 yum clean && \
 rm -r /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
